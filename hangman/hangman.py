83da01a275a89ef1b265536a0681922f97149b3d
cyberace313 <zafer.arifi290@gmail.com>
2023-11-06 11:46:58 +0430

    Added the final touches

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -3,6 +3,8 @@ import random
 import hangman_art
 import hangman_words
 
+logo = hangman_art.logo
+print(logo)
 word_list = hangman_words.word_list
 stages = hangman_art.stages
 lives = 6
@@ -17,6 +19,10 @@ blank_list = []
 for i in range(0, num_of_letters):
     blank_list.append("_")
 
+initial_display = " ".join(blank_list)
+print(initial_display)
+
+
 
 
 #####Receive input and check whether the letter matches any letter in the word#####
@@ -24,18 +30,23 @@ game_over = False
 guesses = []
 while game_over == False:
     guess = input("Guess a letter: ").lower()
+
     if guess in guesses:
         print("You guessed this letter already")
-    guesses.append(guess)
+    else:
+        guesses.append(guess)
+
     for i in range(0, len(chosen_word)):
         if chosen_word[i] == guess:
             blank_list[i] = guess
 
     if guess not in chosen_word:
+        print(f"{guess} is not in the word!")
         lives = lives - 1
         if lives == 0:
             game_over = True
             print("You Lose!")
+            print(f"The word is {chosen_word}")
 
     seperator = " "
     display = seperator.join(blank_list)
@@ -44,6 +55,7 @@ while game_over == False:
         game_over = True
         print("You Win!")
     print(stages[lives])
+
 
 
 
