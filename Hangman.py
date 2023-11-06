14e4c4ad17862f157d4fbe77b2a4ac532ceaa0df
cyberace313 <zafer.arifi290@gmail.com>
2023-11-06 10:06:10 +0430

    Complete step 2

--- Hangman.py
+++ Hangman.py
@@ -8,22 +8,29 @@ word_list = ["aardvark", "baboon", "camel"]
 
 #TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
 
+######Choose a random Word#####
 chosen_word = random.choice(word_list)
-print(chosen_word)
 num_of_letters = len(chosen_word)
 
+#####Display the blank version of the word:#####
 blank_list = []
 for i in range(0, num_of_letters):
     blank_list.append("_")
+
+
+
+#####Receive input and check whether the letter matches any letter in the word#####
+
+guess = input("Guess a letter: ").lower()
+for i in range(0, len(chosen_word)):
+    if chosen_word[i] == guess:
+        blank_list[i] = guess
 
 seperator = " "
 blank = seperator.join(blank_list)
 print(blank)
 
-guess = input("Guess a letter: ").lower()
 
-index = chosen_word.find(guess)
-print(index)
 
 
 
