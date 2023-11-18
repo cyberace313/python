92869f75ab66362614618b3ed14c663a4ce22da7
cyberace313 <zafer.arifi290@gmail.com>
2023-11-18 12:41:46 +0430

    Refactor the logic to incorporate one function for both encoding and decoding

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -1,26 +1,17 @@
-
-def encrypt(text, shift):
-    encrypted_msg = ""
+def caesar_cipher(text, shift, direction):
+    message = ""
+    shifted_position = 0
     for letter in text:
         if letter == " ":
-            encrypted_msg += letter
+            message += letter
         else:
             position = alphabet.index(letter)
-            shifted_position = position + shift
-            encrypted_msg += alphabet[shifted_position]
-    print(f"The encoded text is {encrypted_msg}")
-
-
-def decrypt(text, shift):
-    decrypted_msg = ""
-    for letter in text:
-        if letter == " ":
-            decrypted_msg += letter
-        else:
-            position = alphabet.index(letter)
-            shifted_position = position - shift
-            decrypted_msg += alphabet[shifted_position]
-    print(f"The decoded text is {decrypted_msg}")
+            if direction == "decode":
+                shifted_position = position - shift
+            elif direction == "encode":
+                shifted_position = position + shift
+            message += alphabet[shifted_position]
+    print(f"The {direction}d text is {message}")
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
                 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
@@ -30,7 +21,4 @@ direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-if direction == "encode":
-    encrypt(text, shift)
-elif direction == "decode":
-    decrypt(text, shift)
+caesar_cipher(text, shift, direction)
