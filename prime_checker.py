38a45232a13535845a19988c52df668419d08265
cyberace313 <zafer.arifi290@gmail.com>
2023-11-18 10:48:21 +0430

    Refactor the function

--- prime_checker.py
+++ prime_checker.py
@@ -1,16 +1,12 @@
 def prime_checker(num):
-    if num < 0:
-        print("Please input a positive number")
-    elif num == 0 or num == 1:
-        print("Neither prime nor composite")
-    elif num == 2 or num == 3 or num == 5 or num == 7:
-        print("It is a Prime number")
+    is_prime = True
+    for i in range(2, num):
+        if num % i == 0:
+            is_prime = False
+    if is_prime:
+        print("It is a prime number")
     else:
-        if num % 2 == 0 or num % 3 == 0 or num % 5 == 0 or num % 7 == 0:
-            print("It is not a Prime Number")
-        else:
-            print("It is a Prime Number")
-
+        print("It is not a prime number")
 
 primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
 
