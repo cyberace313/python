a5145e64f75d9abb701dc734b8e6761c56cc4576
cyberace313 <zafer.arifi290@gmail.com>
2023-11-18 15:17:59 +0430

    Modify the function to allow symbols and numbers, add the while loop to keep prompting the user

--- caesar_cipher/caesar_cipher.py
+++ caesar_cipher/caesar_cipher.py
@@ -3,6 +3,8 @@ print(art.logo)
 def caesar_cipher(text, shift, direction):
     message = ""
     shifted_position = 0
+    if shift > 26:
+        shift = shift % 26
     for letter in text:
         if letter == " " or letter not in alphabet:
             message += letter
@@ -19,8 +21,13 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'
                 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
                 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+run = True
 
-caesar_cipher(text, shift, direction)
+while run:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    caesar_cipher(text, shift, direction)
+    run_again = input("Type 'yes' if you want to go again. Otherwise type 'no'.").lower()
+    if run_again == "no":
+        run = False
