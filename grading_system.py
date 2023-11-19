d3c21592f65c001690021366c4e165619645f7f0
cyberace313 <zafer.arifi290@gmail.com>
2023-11-19 09:22:23 +0430

    Initial commit

---
+++ grading_system.py
@@ -0,0 +1,23 @@
+student_scores = {
+    "Harry": 81,
+    "Ron": 78,
+    "Hermione": 99,
+    "Draco": 74,
+    "Neville": 62,
+}
+student_grades = {}
+
+grade = ""
+for student in student_scores:
+    score = student_scores[student]
+    if score > 90:
+        grade = "Outstanding"
+    elif score > 80:
+        grade = "Exceeds Expectations"
+    elif score > 70:
+        grade = "Acceptable"
+    elif score <= 70:
+        grade = "Fail"
+    student_grades[student] = grade
+
+print(student_grades)
