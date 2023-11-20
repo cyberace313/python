603159af2de3b1a06531192b887678c5ce1b195e
cyberace313 <zafer.arifi290@gmail.com>
2023-11-20 12:10:24 +0430

    Modify the code to incorporate dictionary

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -1,42 +1,50 @@
 from art import logo
 print(logo)
 
-def calculate(a, b):
-    if operation == "+":
-        return a + b
-    elif operation == "-":
-        return a - b
-    elif operation == "*":
-        return a * b
-    elif operation == "/":
-        return a / b
+def add (n1, n2):
+    return n1 + n2
+
+def subtract (n1, n2):
+    return n1 - n2
+
+def multiply (n1, n2):
+    return n1 * n2
+
+def divide (n1, n2):
+    return n1 / n2
+
+operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide,
+}
 
 calc = True
 while calc:
-    first_num = float(input("What is the first number? "))
-    operation = input("""
-    +
-    -
-    *
-    /
-    Pick and Operation """)
-    second_num = float(input("What is the second number? "))
-    result = calculate(first_num, second_num)
-    print(f"{first_num} {operation} {second_num} = {result}")
+    num1 = float(input("What is the first number? "))
+    for key in operations:
+        print(key)
+    operation_symbol = input("Pick an Operation ")
+    operation = operations[operation_symbol]
+    num2 = float(input("What is the second number? "))
+    result = operation(num1, num2)
+    print(f"{num1} {operation_symbol} {num2} = {result}")
     continue_calc = True
     while continue_calc:
-        keep_going = input(f"Type y to continue calculation with {result}, or type 'n' to start a new calculation or type 'e' to exit the calculator ")
+        keep_going = input(
+            f"Type y to continue calculation with {result}, or type 'n' to start a new calculation or type 'e' to exit the calculator ")
         if keep_going == "y":
-            first_num = result
-            operation = input("""
-              +
-              -
-              *
-              /
-              Pick and Operation """)
-            second_num = float(input("What is the second number? "))
-            result = calculate(first_num, second_num)
-            print(f"{first_num} {operation} {second_num} = {result}")
+            num1 = result
+            for key in operations:
+                print(key)
+            operation_symbol = input("Pick an Operation ")
+            operation = operations[operation_symbol]
+            num2 = float(input("What is the second number? "))
+            result = operation(num1, num2)
+            print(f"{num1} {operation_symbol} {num2} = {result}")
+        elif keep_going == "n":
+            continue_calc = False
         elif keep_going == "n":
             continue_calc = False
         elif keep_going == "e":
