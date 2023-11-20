f3cd9b52fb5d8446d7056b6d1f8977c094cd623f
cyberace313 <zafer.arifi290@gmail.com>
2023-11-20 11:47:49 +0430

    Modify the function to combine all the operations and add while loop to keep calculating

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -1,38 +1,46 @@
 from art import logo
 print(logo)
 
-#Operations:
-def add(a, b):
-    """Performs addition between two given numbers"""
-    return a + b
-def subtract(a, b):
-    """Performs subtractions between two given numbers"""
-    return a - b
-def multiply(a, b):
-    """Performs multiplication between two given numbers"""
-    return a * b
-def divide(a, b):
-    """Performs division between two given numbers"""
-    return a / b
+def calculate(a, b):
+    if operation == "+":
+        return a + b
+    elif operation == "-":
+        return a - b
+    elif operation == "*":
+        return a * b
+    elif operation == "/":
+        return a / b
 
-a = int(input("What is the first number? "))
-
-operation = input("""
-+
--
-*
-/
-Pick and Operation """)
-b = int(input("What is the second number? "))
-
-result = 0
-if operation == "+":
-    result = add(a, b)
-elif operation == "-":
-    result = subtract(a, b)
-elif operation == "*":
-    result = multiply(a, b)
-elif operation == "/":
-    result = divide(a, b)
-
-print(result)
+calc = True
+while calc:
+    first_num = float(input("What is the first number? "))
+    operation = input("""
+    +
+    -
+    *
+    /
+    Pick and Operation """)
+    second_num = float(input("What is the second number? "))
+    result = calculate(first_num, second_num)
+    print(f"{first_num} {operation} {second_num} = {result}")
+    continue_calc = True
+    while continue_calc:
+        keep_going = input(f"Type y to continue calculation with {result}, or type 'n' to start a new calculation or type 'e' to exit the calculator ")
+        if keep_going == "y":
+            first_num = result
+            operation = input("""
+              +
+              -
+              *
+              /
+              Pick and Operation """)
+            second_num = float(input("What is the second number? "))
+            result = calculate(first_num, second_num)
+            print(f"{first_num} {operation} {second_num} = {result}")
+        elif keep_going == "n":
+            continue_calc = False
+        elif keep_going == "e":
+            continue_calc = False
+            calc = False
+        else:
+            print("Invalid Input")
