5bdc6b3282655870ee7de32284d242f3004054c4
cyberace313 <zafer.arifi290@gmail.com>
2023-11-20 12:29:11 +0430

    Modify to code to incorporate recursion

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -19,36 +19,25 @@ operations = {
     "*": multiply,
     "/": divide,
 }
-
-calc = True
-while calc:
+def calculator():
     num1 = float(input("What is the first number? "))
     for key in operations:
         print(key)
-    operation_symbol = input("Pick an Operation ")
-    operation = operations[operation_symbol]
-    num2 = float(input("What is the second number? "))
-    result = operation(num1, num2)
-    print(f"{num1} {operation_symbol} {num2} = {result}")
     continue_calc = True
     while continue_calc:
+        operation_symbol = input("Pick an Operation ")
+        operation = operations[operation_symbol]
+        num2 = float(input("What is the second number? "))
+        result = operation(num1, num2)
+        print(f"{num1} {operation_symbol} {num2} = {result}")
         keep_going = input(
-            f"Type y to continue calculation with {result}, or type 'n' to start a new calculation or type 'e' to exit the calculator ")
+            f"Type y to continue calculation with {result}, or type 'n' to start a new calculation ")
         if keep_going == "y":
             num1 = result
-            for key in operations:
-                print(key)
-            operation_symbol = input("Pick an Operation ")
-            operation = operations[operation_symbol]
-            num2 = float(input("What is the second number? "))
-            result = operation(num1, num2)
-            print(f"{num1} {operation_symbol} {num2} = {result}")
         elif keep_going == "n":
             continue_calc = False
-        elif keep_going == "n":
-            continue_calc = False
-        elif keep_going == "e":
-            continue_calc = False
-            calc = False
+            calculator()
         else:
             print("Invalid Input")
+
+calculator()
