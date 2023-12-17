b7adbeb3d6827c1a885442f93ecd77398a5daacf
cyberace313 <zafer.arifi290@gmail.com>
2023-12-17 11:37:32 +0430

    Move to corresponding subdirectory

--- random_buyer.py
+++
@@ -1,8 +0,0 @@
-import random
-
-names = input("Please input the names of the people seperated by a comma and space eg: Sam, John, Alex")
-names_list = names.split(", ")
-
-random_person = random.choice(names_list)
-
-print(f"{random_person} will pay for the meal!")
